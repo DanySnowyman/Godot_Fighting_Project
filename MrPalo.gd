690134dec6eeb45ee653e60a1f4e3fc8241f748a
DanySnowyman <danysnowyman@gmail.com>
2020-10-01 19:08:27 +0200

    Dashes agregados

--- MrPalo.gd
+++ MrPalo.gd
@@ -11,6 +11,8 @@ var is_jumping = false
 var jump_vertical = false
 var jump_forward = false
 var jump_backward = false
+var dash_f_ready = false
+var dash_b_ready = false
 
 onready var tween = get_node("Tween")
 
@@ -18,6 +20,10 @@ const JUMP_HEIGHT = 80
 const JUMP_LENGHT = 120
 const JUMP_ASC_TIME = 0.4
 const JUMP_DES_TIME = 0.4
+const DASH_F_DIST = 80
+const DASH_B_DIST = 60
+const DASH_F_TIME = 0.4
+const DASH_B_TIME = 0.4
 
 func _ready():
 	if player == 1:
@@ -26,7 +32,6 @@ func _ready():
 	
 	$AnimationPlayer.play("Stand")
 
-
 func walk_forward(delta):
 	if Input.is_action_pressed("ui_right") and on_floor == true:
 		$AnimationPlayer.play("Walk forward")
@@ -34,7 +39,7 @@ func walk_forward(delta):
 		
 	if Input.is_action_just_released("ui_right") and on_floor == true:
 		$AnimationPlayer.play("Stand")
-	
+
 func walk_backwards(delta):
 	if Input.is_action_pressed("ui_left") and on_floor == true:
 		$AnimationPlayer.play("Walk backwards")
@@ -72,7 +77,6 @@ func jump_vertical():
 			JUMP_DES_TIME, Tween.TRANS_QUINT, Tween.EASE_IN)
 		tween.start()
 		yield(tween, "tween_completed")
-		yield($AnimationPlayer, "animation_finished")
 		is_jumping = false
 		on_floor = true
 		$AnimationPlayer.play("Stand")
@@ -133,6 +137,47 @@ func jump_backward():
 		on_floor = true
 		$AnimationPlayer.play("Stand")
 
+func dash_forward():
+	if Input.is_action_just_pressed("ui_right") and dash_f_ready == false:
+		dash_f_ready = true
+		dash_b_ready = false
+		yield(get_tree().create_timer(0.4), "timeout")
+		dash_f_ready = false
+		
+	if on_floor == true and is_jumping == false:
+		if Input.is_action_just_pressed("ui_right") and dash_f_ready == true:
+			on_floor = false
+			$AnimationPlayer.play("Dash forward")
+			tween.interpolate_property(self, "position:x", self.position.x,
+				self.position.x + DASH_F_DIST, DASH_F_TIME,
+				Tween.TRANS_QUART, Tween.EASE_OUT)
+			tween.start()
+			yield(tween, "tween_completed")
+			dash_f_ready = false
+			on_floor = true
+			$AnimationPlayer.play("Stand")
+			
+func dash_backward():
+	if Input.is_action_just_pressed("ui_left") and dash_b_ready == false:
+		dash_b_ready = true
+		dash_f_ready = false
+		yield(get_tree().create_timer(0.4), "timeout")
+		dash_b_ready = false
+		
+	if on_floor == true and is_jumping == false:
+		if Input.is_action_just_pressed("ui_left") and dash_b_ready == true:
+			on_floor = false
+			$AnimationPlayer.play("Dash backward")
+			tween.interpolate_property(self, "position:x", self.position.x,
+				self.position.x - DASH_B_DIST, DASH_B_TIME,
+				Tween.TRANS_QUINT, Tween.EASE_OUT)
+			tween.start()
+			yield(tween, "tween_completed")
+			dash_b_ready = false
+			on_floor = true
+			$AnimationPlayer.play("Stand")
+
+
 func _process(delta):
 	var motion = Vector2()
 	
@@ -142,10 +187,9 @@ func _process(delta):
 	jump_vertical()
 	jump_forward()
 	jump_backward()
+	dash_forward()
+	dash_backward()
 	
 	motion = position - last_position
 	last_position = position
-	
-	print(is_jumping)
-	
 	
