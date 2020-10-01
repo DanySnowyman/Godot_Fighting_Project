14b39e105eba40d5e541c4a0f762cfcb4b9ac81d
DanySnowyman <danysnowyman@gmail.com>
2020-10-01 17:51:12 +0200

    Salto en 3 direcciones listo

--- MrPalo.gd
+++ MrPalo.gd
@@ -8,6 +8,9 @@ var last_position = Vector2()
 
 var on_floor = true
 var is_jumping = false
+var jump_vertical = false
+var jump_forward = false
+var jump_backward = false
 
 onready var tween = get_node("Tween")
 
@@ -39,68 +42,106 @@ func walk_backwards(delta):
 		
 	if Input.is_action_just_released("ui_left") and on_floor == true:
 		$AnimationPlayer.play("Stand")
-		
-func jump_vertical():
+
+func jump_selector():
 	if on_floor == true and is_jumping == false:
 		if Input.is_action_pressed("ui_up"):
+			on_floor = false
 			is_jumping = true
 			yield(get_tree().create_timer(0.05), "timeout")
-			on_floor = false
-			$AnimationPlayer.play("Jump vertical")
-			yield(get_tree().create_timer(0.1), "timeout")
-			tween.interpolate_property(self, "position:y",
-				self.position.y, (self.position.y - JUMP_HEIGHT),
-				JUMP_ASC_TIME, Tween.TRANS_QUINT, Tween.EASE_OUT)
-			tween.start()
-			yield(tween, "tween_completed")
-			tween.interpolate_property(self, "position:y",
-				self.position.y, (self.position.y + JUMP_HEIGHT),
-				JUMP_DES_TIME, Tween.TRANS_QUINT, Tween.EASE_IN)
-			tween.start()
-			yield(tween, "tween_completed")
-			yield($AnimationPlayer, "animation_finished")
-			on_floor = true
-			is_jumping = false
-			$AnimationPlayer.play("Stand")
-		
+			if Input.is_action_pressed("ui_right"):
+				jump_forward = true
+			elif Input.is_action_pressed("ui_left"):
+				jump_backward = true
+			else: jump_vertical = true
+
+func jump_vertical():
+	if jump_vertical == true:
+		jump_vertical = false
+		$AnimationPlayer.play("Jump vertical")
+		yield(get_tree().create_timer(0.1), "timeout")
+		#Arco ascendente --------------------------------
+		tween.interpolate_property(self, "position:y",
+			self.position.y, (self.position.y - JUMP_HEIGHT),
+			JUMP_ASC_TIME, Tween.TRANS_QUINT, Tween.EASE_OUT)
+		tween.start()
+		yield(tween, "tween_completed")
+		#Arco descendente -------------------------------
+		tween.interpolate_property(self, "position:y",
+			self.position.y, (self.position.y + JUMP_HEIGHT),
+			JUMP_DES_TIME, Tween.TRANS_QUINT, Tween.EASE_IN)
+		tween.start()
+		yield(tween, "tween_completed")
+		yield($AnimationPlayer, "animation_finished")
+		is_jumping = false
+		on_floor = true
+		$AnimationPlayer.play("Stand")
+
 func jump_forward():
-	if on_floor == true:
-		if Input.is_action_pressed("ui_right") and\
-		Input.is_action_pressed("ui_up"):
-			is_jumping = true
-			on_floor = false
-			print("Forwardjump!")
-			#Arco ascendente --------------------------------
-			$AnimationPlayer.play("Jump forward")
-			yield(get_tree().create_timer(0.1), "timeout")
-			tween.interpolate_property(self, "position:x",
-				self.position.x, self.position.x + JUMP_LENGHT / 2,
-				JUMP_ASC_TIME, Tween.TRANS_LINEAR, Tween.EASE_OUT)
-			tween.interpolate_property(self, "position:y",
+	if jump_forward == true:
+		jump_forward = false
+		$AnimationPlayer.play("Jump forward")
+		yield(get_tree().create_timer(0.1), "timeout")
+		#Arco ascendente --------------------------------
+		tween.interpolate_property(self, "position:x",
+			self.position.x, self.position.x + JUMP_LENGHT / 2,
+			JUMP_ASC_TIME, Tween.TRANS_LINEAR, Tween.EASE_OUT)
+		tween.interpolate_property(self, "position:y",
 			self.position.y, self.position.y - JUMP_HEIGHT,
 			JUMP_DES_TIME, Tween.TRANS_QUINT, Tween.EASE_OUT)
-			yield(tween, "tween_completed")
-			#Arco descendente -------------------------------
-			tween.interpolate_property(self, "position:x",
-				self.position.x, self.position.x + JUMP_LENGHT / 2,
-				JUMP_DES_TIME, Tween.TRANS_LINEAR, Tween.EASE_IN)
-			tween.interpolate_property(self, "position:y",
-				self.position.y, self.position.y + JUMP_HEIGHT,
-				JUMP_DES_TIME, Tween.TRANS_QUINT, Tween.EASE_IN)
-			tween.start()
-			yield(tween, "tween_completed")
-			yield($AnimationPlayer, "animation_finished")
-			is_jumping = false
-			on_floor = true
-			$AnimationPlayer.play("Stand")
+		tween.start()
+		yield(tween, "tween_completed")
+#		#Arco descendente -------------------------------
+		tween.interpolate_property(self, "position:x",
+			self.position.x, self.position.x + JUMP_LENGHT / 2,
+			JUMP_DES_TIME, Tween.TRANS_LINEAR, Tween.EASE_IN)
+		tween.interpolate_property(self, "position:y",
+			self.position.y, self.position.y + JUMP_HEIGHT,
+			JUMP_DES_TIME, Tween.TRANS_QUINT, Tween.EASE_IN)
+		tween.start()
+		yield(tween, "tween_completed")
+		yield($AnimationPlayer, "animation_finished")
+		is_jumping = false
+		on_floor = true
+		$AnimationPlayer.play("Stand")
+
+func jump_backward():
+	if jump_backward == true:
+		jump_backward = false
+		$AnimationPlayer.play("Jump backward")
+		yield(get_tree().create_timer(0.1), "timeout")
+		#Arco ascendente --------------------------------
+		tween.interpolate_property(self, "position:x",
+			self.position.x, self.position.x - JUMP_LENGHT / 2,
+			JUMP_ASC_TIME, Tween.TRANS_LINEAR, Tween.EASE_OUT)
+		tween.interpolate_property(self, "position:y",
+			self.position.y, self.position.y - JUMP_HEIGHT,
+			JUMP_DES_TIME, Tween.TRANS_QUINT, Tween.EASE_OUT)
+		tween.start()
+		yield(tween, "tween_completed")
+#		#Arco descendente -------------------------------
+		tween.interpolate_property(self, "position:x",
+			self.position.x, self.position.x - JUMP_LENGHT / 2,
+			JUMP_DES_TIME, Tween.TRANS_LINEAR, Tween.EASE_IN)
+		tween.interpolate_property(self, "position:y",
+			self.position.y, self.position.y + JUMP_HEIGHT,
+			JUMP_DES_TIME, Tween.TRANS_QUINT, Tween.EASE_IN)
+		tween.start()
+		yield(tween, "tween_completed")
+		yield($AnimationPlayer, "animation_finished")
+		is_jumping = false
+		on_floor = true
+		$AnimationPlayer.play("Stand")
 
 func _process(delta):
 	var motion = Vector2()
 	
 	walk_forward(delta)
 	walk_backwards(delta)
+	jump_selector()
 	jump_vertical()
 	jump_forward()
+	jump_backward()
 	
 	motion = position - last_position
 	last_position = position
